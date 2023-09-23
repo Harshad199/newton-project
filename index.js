63f9a72d9357990a97a482b24c7bf02fee6f6084
Harshad199 <harshadgajbhiye151119@gmail.com>
2023-09-23 20:58:28 +0530

    add new File

--- index.js
+++
@@ -1,9 +0,0 @@
-index.js
-function Sayhellow(x){
-    console.log(x)
-}
-var Sayhellow = function(y){
-    console.log(y)
-
-}
-Sayhellow("harshad")
--- intro.java
+++
@@ -1,6 +0,0 @@
-intro.java
-Class Main{
-    public static void main(String args){
-        System.out.println("hellow world");
-    }
-}
